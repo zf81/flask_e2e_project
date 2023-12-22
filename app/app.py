b3f0c1233aabc764ec791acf1b277f3a5f176f5f
zf81 <fizzah.zaidi@stonybrook.edu>
2023-12-22 02:34:49 +0000

    update

--- app/app.py
+++ app/app.py
@@ -58,7 +58,7 @@ def google():
         redirect_uri = url_for('google_auth', _external=True)
         print('REDIRECT URL: ', redirect_uri)
         session['nonce'] = generate_token()
-        redirect_uri = 'https://5000-cs-95785c58-446f-40ea-8544-2b7d3521503b.cs-us-east1-pkhd.cloudshell.dev/google/auth/'
+        redirect_uri = 'https://5000-cs-21160185-bd7b-4ea1-8b97-7e676f850e7a.cs-us-east1-rtep.cloudshell.dev/google/auth/'
         return oauth.google.authorize_redirect(redirect_uri, nonce=session['nonce'])
     except Exception as e:
         logging.error(f"an error occured! {e}")
